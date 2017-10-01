88f9bc5932ddddebdeabd2c432c02fbdf4140fc4
Sam Chami <Sam@Michaels-MacBook-Pro.local>
2017-10-01 15:00:06 -0700

    Added stripQuotes() and scramble()

--- homework2/warmup.py
+++ homework2/warmup.py
@@ -1,5 +1,20 @@
+from random import shuffle
+
+
 def change(c):
     quarters = divmod(c, 25)
     dimes = divmod(quarters[1], 10)
     nickles = divmod(dimes[1], 5)
     return quarters[0], dimes[0], nickles[0], nickles[1]
+
+
+def stripQuotes(quote):
+    quote = quote.replace('\'', '')
+    quote = quote.replace('"', '')
+    return quote
+
+
+def scramble(s):
+    s = list(s)
+    shuffle(s)
+    return ''.join(s)
