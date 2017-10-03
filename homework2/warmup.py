ad255e50bf30863cf7318551a666ae3653821563
JohnS <John@Johns-MacBook-Pro.local>
2017-10-02 17:03:09 -0700

    Modified warmup.py in homework2

--- homework2/warmup.py
+++ homework2/warmup.py
@@ -1,2 +1,33 @@
+import random
+import re
+
 def change(c):
-    
+    if amount < 0:
+        rasie()
+
+def strip_quotes(c):
+    re.sub
+
+def scramble(c):
+    join(random.Sample())
+
+def say(c):
+    return c
+
+def triples(c):
+    return c
+
+def powers(c):
+    return c
+
+def interleave(c):
+    return c
+
+def Cylinder(c):
+    return c
+
+def make_crypto_functions(c):
+    return c
+
+def random_name(c):
+    return c
