1bce33c797422368215ff9f43f787db40ef07dc4
Sam Chami <Sam@Michaels-MacBook-Pro.local>
2017-11-13 23:05:06 -0800

    Part of say function. Incomplete

---
+++ homework3/say.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+
+using namespace std;
+// auto lambda = [](int x, int y) -> int { return x + y; };
+// auto say = std::bind(lambda, 5, std::placeholders::_1);
+int say (int x) {
+    return x;
+};
+
+//
+string say (string firstWord) {
+    if (firstWord.empty()) {
+        return firstWord;
+    }
+    string sayNext = (string secondWord) {
+        if(firstWord.empty()) {
+            return firstWord;
+        }
+        return say(firstWord + " " + secondWord);
+    };
+    return sayNext;
+};
+//
+
+int main () {
+}
+//     assert(say() == "");
+//     assert(say("hey") == "hey");
+//     assert(say("I")("was")("doing")("just")("fine") == "I was doing just fine");
+//     assert(say("before")("I")("met")("you")) == "before I met you");
+// }
