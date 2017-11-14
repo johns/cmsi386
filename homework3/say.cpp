854c0713f067919dce9cdfb07e1424e24bf4af7d
Sam Chami <Sam@Michaels-MacBook-Pro.local>
2017-11-14 02:05:29 -0800

    Tried say

--- homework3/say.cpp
+++ homework3/say.cpp
@@ -5,24 +5,42 @@
 using namespace std;
 // auto lambda = [](int x, int y) -> int { return x + y; };
 // auto say = std::bind(lambda, 5, std::placeholders::_1);
-int say (int x) {
-    return x;
-};
+// int say (int x) {
+//     return x;
+// };
 
+
+auto say = curry ( [](auto x, auto y, auto z) { return x + y + z} );
+
+// _curry(std::function<R(T,Ts...)> fun)
+// : result (
+//     [=](const T& t) {
+//         return _curry<std::function<R(Ts...)>>(
+//             [=](const Ts&...ts){
+//                 return fun(t, ts...);
+//             }
+//         ).result;
+//     }
+// ) {}
+
+
+
+
+
+// //
+// string say (string firstWord) {
+//     if (firstWord.empty()) {
+//           return firstWord;
+//     }
 //
-string say (string firstWord) {
-    if (firstWord.empty()) {
-        return firstWord;
-    }
-    string sayNext = (string secondWord) {
-        if(firstWord.empty()) {
-            return firstWord;
-        }
-        return say(firstWord + " " + secondWord);
-    };
-    return sayNext;
-};
-//
+//     return [firstWord](string secondWord) {
+//         if(secondWord.empty()) {
+//             return firstWord;
+//         }
+//         return say(firstWord + " " + secondWord);
+//     };
+// };
+
 
 int main () {
 }
