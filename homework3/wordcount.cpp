41ce5941f615b0d780384362caac77b9e610480d
JohnS <John@Johns-MacBook-Pro.local>
2017-11-14 13:26:36 -0800

    Got rid of implementation of wordcount that did not work and fomatting changes

--- homework3/wordcount.cpp
+++ homework3/wordcount.cpp
@@ -1,43 +1,3 @@
-/*#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-
-using namespace std;
-
-struct Sentence {
-  int n = 0;
-  string s = "";
-};
-
-Sentence * wordcount_from_file(string filename) {
-  ifstream file;
-  file.open(filename.c_str());
-  if (!file) {
-    throw string("File '" + filename + "' not found");
-  }
-  static Sentence phrase[];
-  //to do
-  return phrase;
-}
-
-int main() {
-  try {
-    string filename;
-    cout << "What text file do you want to find the wordcounts of? ";
-    cin >> filename;
-    Sentence *words;
-    words = wordcount_from_file(filename);
-    int i = 0;
-    while (i < words.size()) {
-      cout << *(words + i).s << " " << *(words + i).n << '\n';
-    }
-  } catch (string s) {
-    cerr << "Error: " << s << "\n";
-    return -1;
-  }
-}*/
-
 #include <iterator>
 #include <iostream>
 #include <fstream>
@@ -69,7 +29,7 @@ int main() {
   ifstream fin(filename);
 
   string s;
-  while((s = getNextChar(fin))!= "" )
+  while((s = getNextChar(fin))!= "")
     ++words[s];
 
   for(map<string,int>::iterator iter = words.begin(); iter != words.end(); ++iter)
