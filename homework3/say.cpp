9bd9a431c1e0f27ca22a1afc7d8e1fe54f701de4
Benjamin M Smith <31645455+BenProtusSmith@users.noreply.github.com>
2017-11-14 02:07:40 -0800

    Working say function
    Modified from @rtoal's Adder function.

--- homework3/say.cpp
+++ homework3/say.cpp
@@ -3,31 +3,61 @@
 #include <string>
 
 using namespace std;
-// auto lambda = [](int x, int y) -> int { return x + y; };
-// auto say = std::bind(lambda, 5, std::placeholders::_1);
-int say (int x) {
-    return x;
-};
 
-//
-string say (string firstWord) {
-  if (firstWord.empty()) {
-    return firstWord;
+struct Adder {
+private:
+  string result = "";
+public:
+  Adder(string s = ""): result(s) {}
+  
+  Adder operator()(string word) {
+    return Adder(result + " " + word);
   }
-  string sayNext = (string secondWord) {
-    if(firstWord.empty()) {
-      return firstWord;
+  
+  string operator()() {
+    if (result.length()) {
+      result = result.substr(1);
     }
-    return say(firstWord + " " + secondWord);
-  };
-  return sayNext;
+    return result;
+  }
 };
-//
 
-int main () {
+Adder f;
+
+int main() {
+  assert(f() == "");
+  assert(f("Hi!")() == "Hi!");
+  assert(f("Hello")("there")() == "Hello there");
+  assert(f("My")("name")("is")("Jeff")() == "My name is Jeff");
+  assert(f(" ")() == " ");
+  cout << "All tests passed!\n";
 }
-//     assert(say() == "");
-//     assert(say("hey") == "hey");
-//     assert(say("I")("was")("doing")("just")("fine") == "I was doing just fine");
-//     assert(say("before")("I")("met")("you")) == "before I met you");
+
+// // auto lambda = [](int x, int y) -> int { return x + y; };
+// // auto say = std::bind(lambda, 5, std::placeholders::_1);
+// int say (int x) {
+//     return x;
+// };
+
+// //
+// string say (string firstWord) {
+//   if (firstWord.empty()) {
+//     return firstWord;
+//   }
+//   string sayNext = (string secondWord) {
+//     if(firstWord.empty()) {
+//       return firstWord;
+//     }
+//     return say(firstWord + " " + secondWord);
+//   };
+//   return sayNext;
+// };
+// //
+
+// int main () {
 // }
+// //     assert(say() == "");
+// //     assert(say("hey") == "hey");
+// //     assert(say("I")("was")("doing")("just")("fine") == "I was doing just fine");
+// //     assert(say("before")("I")("met")("you")) == "before I met you");
+// // }
