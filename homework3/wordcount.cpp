346ed174cbcfdd378b86eaf4a99ddf3119cfeea9
JohnS <John@Johns-MacBook-Pro.local>
2017-11-14 13:22:10 -0800

    wordcount implementation almost done

--- homework3/wordcount.cpp
+++ homework3/wordcount.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
@@ -36,4 +36,42 @@ int main() {
     cerr << "Error: " << s << "\n";
     return -1;
   }
+}*/
+
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+string getNextChar(istream &in) {
+  char c;
+  string ans = "";
+  c = in.get();
+  while(!isalpha(c) && !in.eof()) {
+    c = in.get();
+  }
+  while(isalpha(c)) {
+    ans.push_back(tolower(c));
+    c = in.get();
+  }
+  return ans;
+}
+
+int main() {
+  string filename;
+  cout << "What text file do you want to find the wordcounts of? ";
+  cin >> filename;
+  map<string,int> words;
+  ifstream fin(filename);
+
+  string s;
+  while((s = getNextChar(fin))!= "" )
+    ++words[s];
+
+  for(map<string,int>::iterator iter = words.begin(); iter != words.end(); ++iter)
+    cout << iter->first << " " << iter->second << endl;
 }
