db8aa20461b8414e7944d3e976bde4530a8dc8d5
JohnS <johnrscott4@gmail.com>
2017-12-06 02:24:12 -0800

    Added stripQuotes function to Warmup.elm

--- homework4/Warmup.elm
+++ homework4/Warmup.elm
@@ -1,5 +1,6 @@
 module Warmup exposing (..)
 
+import String exposing (join, split)
 
 change : Int -> Result String ( Int, Int, Int, Int )
 change num =
@@ -7,3 +8,7 @@ change num =
         Err "amount cannot be negative"
     else
         Ok (num // 25, num % 25 // 10, num % 25 % 10 // 5, num % 25 % 10 % 5)
+
+stripQuotes : String -> String
+stripQuotes str =
+    join "" (split "'" (join "" (split "\"" str)))
